86ec1f107a0d55069595d1ab56fa9b717873fe7f
Tony Christopher <tony_christopher@shelter.org.uk>
2021-05-16 18:04:31 +0100

    Added avatars

--- resources/views/components/user-card.blade.php
+++ resources/views/components/user-card.blade.php
@@ -3,5 +3,5 @@
     <a href="{{ route('logout') }}" class="link-btn">Log Out</a>
 </div>
 <div class="user-card-left">
-    <img src="https://via.placeholder.com/72" alt="{{ Auth::user()->name }} user avatar" class="user-avatar" />
+    <img src="{{ URL::asset('images/avatars/' . Auth::user()->avatar) }}" alt="{{ Auth::user()->name }} user avatar" class="user-avatar" />
 </div>
